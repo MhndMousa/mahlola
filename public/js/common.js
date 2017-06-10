bc4a10c9559f6fb553dbee0816b9e1b5ca807a2e
Mhnd Mousa <mhndmsa@gmail.com>
2017-06-10 02:19:37 -0400

    added local libraries for faster recall

--- public/js/common.js
+++ public/js/common.js
@@ -13,10 +13,11 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
 // Global colors
 var colors = {
   text : "grey-text ",
-  primary : "cyan darken-3 ",
+  primary : "orange darken-4",
   textAccent : "text-lighten-1 ",
   textAccentHeader : "text-lighten-4 "
 }
@@ -24,39 +25,48 @@ var colors = {
 
 // Formats Header
 function getHeader(){
-  $('header').append(
+  $('mheader').append(
     $('<nav/>').append(
-      $('<div/>',{"class": function(){return "nav-wrapper " + colors.primary}}).append([
+      $('<div/>',{"class": "nav-wrapper " + colors.primary}).append([
         // Logo Picture
         $('<a/>',{href: "#", class: "center brand-logo"}).html("Logo"),
 
-        // Mobile Menu button
-        $('<a/>',{href: "#", "data-activates": "slide-out-menu", class: "button-collapse"}).append(
+        /*
+        Menu
+        */
+        // Menu button
+        $('<a/>',{href: "#", "data-activates": "slide-out-menu",id:"slide-out-button", class: "button-collapse show-on-large"}).append(
           $('<i/>', {class: "material-icons"}).html("menu")
         ),
+        // TODO: Hide user menu for not logged in users
         // User mobile menu
-        $('<ul/>',{ id: "slide-out-menu", class: function(){return "side-nav "}}).append([
+        $('<ul/>',{ id: "slide-out-menu", class: " side-nav"}).append([
           $('<li/>').append(
             $('<div/>', {class:"userView"}).append([
             $('<div/>', {class:"background"}).append($('<img/>' ,{src: "http://oxygennacdn3.oxygenna.com/wp-content/uploads/2015/11/18.jpg"})),
-            $('<a/>').append($('<img/>', {class: "circle" ,src:"https://cdn.dribbble.com/users/869811/screenshots/3105584/dribbble_shot_12_1x.png"})),
-            $('<a/>').append($('<span/>',{class: "white-text name" }).html("Muhannad Alnemer")),
-            $('<a/>').append($('<span/>',{class: "white-text email" }).html("Muhannad Alnemer")),
+            $('<a/>').append($('<img/>', {class: "circle", id:"user_photo" ,src:"https://cdn.dribbble.com/users/869811/screenshots/3105584/dribbble_shot_12_1x.png"})),
+            $('<a/>').append($('<span/>',{class: "white-text name", id:"user_name"}).html("not mhnd")),
+            $('<a/>').append($('<span/>',{class: "white-text email", id:"user_email" }).html("Muhannad Alnemer")),
           ])),
           $('<li/>').append($('<a/>',{ href: "https://www.google.com", class: "waves-effect "}).html("google")),
           $('<li/>').append($('<a/>',{ href: "https://www.yahoo.com", class: "waves-effect "}).html("yahoo")),
           $('<li/>').append($('<a/>',{ href: "https://www.facebook.com", class: "waves-effect "}).html("facebook"))
         ]),
 
-
+        /*
+        Drop Down
+        */
         // User dropdown button
         $('<ul/>',{class: "right hide-on-med-and-down "}).append([
-          $('<li/>').append($('<a/>',{ href: "#!", "data-activates": "dropdown1", class: function(){return "dropdown-button " + colors.text + colors.textAccentHeader}}).html("User").append($('<i/>', {class: "material-icons right"}).html("arrow_drop_down"))
-          )
+          $('<li/>').append($('<a/>',{ href: "#!", "data-activates": "dropdown1", class: "dropdown-button "+ colors.text + colors.textAccentHeader}).html($('<i/>', {class: "material-icons"}).html("account_circle")).append([
+            // $('<a/>').append($('<img/>', {class: "circle" ,src:"https://cdn.dribbble.com/users/869811/screenshots/3105584/dribbble_shot_12_1x.png"})),
+            // $('<i/>', {class: "material-icons right"}).html("arrow_drop_down")
+          ]))
         ]),
         // User dropdown menu
-        $('<ul/>',{ id: "dropdown1", class: "dropdown-content "}).append([
-          $('<li/>',{class: "divider" }),
+        $('<ul/>',{ id: "dropdown1", class: "dropdown-content"}).append([
+          // $('<li/>').append($('<a/>',{class: "red-text text-lighten-1" , id:"user_name"}).html("something")),
+          $('<li/>',{class: "divider"}),
           $('<li/>').append($('<a/>',{class: "red-text text-lighten-1" , id:"logout_button"}).html("تسجيل خروج"))
         ])
       ])
@@ -66,28 +76,28 @@ function getHeader(){
 
 // Formats footer
 function getFooter(){
-  $('blah').append(
-    $('<footer/>',{ class : function(){return "page-footer " + colors.primary }} ).append([
+  $('mFooter').append(
+    $('<footer/>',{ class : "page-footer " + "grey darken-2"} ).append([
       $('<div/>', { class: "container" }).append(
         $('<div/>', {class : "row"}).append([
           $('<div/>', {class: "col l6 s12"}).append([
-            $('<h5/>', {class:  function(){return colors.text + colors.textAccentHeader}}).html("Footer Content"),
-            $('<p/>', {class:  function(){return colors.text + colors.textAccent }}).html("You can use rows and columns here to organize your footer content")
+            $('<h5/>', {class:  colors.text + colors.textAccentHeader}).html("Footer Content"),
+            $('<p/>', {class:  colors.text + colors.textAccent }).html("You can use rows and columns here to organize your footer content")
           ]),
           $('<div/>', {class: "col l4 offset-l2 s12"}).append([
-            $('<h5/>', {class:  function(){return colors.text +colors.textAccentHeader}}).html("Links"),
+            $('<h5/>', {class:  colors.text +colors.textAccentHeader}).html("Links"),
             $('<ul>').append([
-              $('<li/>').append($('<a/>',{class: function(){return colors.text + colors.textAccent}, href: "#!" }).html("link 1")),
-              $('<li/>').append($('<a/>',{class: function(){return colors.text + colors.textAccent}, href: "#!" }).html("link 2")),
-              $('<li/>').append($('<a/>',{class: function(){return colors.text + colors.textAccent}, href: "#!" }).html("link 3")),
-              $('<li/>').append($('<a/>',{class: function(){return colors.text + colors.textAccent}, href: "#!" }).html("link 4"))
+              $('<li/>').append($('<a/>',{class: colors.text + colors.textAccent, href: "#!" }).html("link 1")),
+              $('<li/>').append($('<a/>',{class: colors.text + colors.textAccent, href: "#!" }).html("link 2")),
+              $('<li/>').append($('<a/>',{class: colors.text + colors.textAccent, href: "#!" }).html("link 3")),
+              $('<li/>').append($('<a/>',{class: colors.text + colors.textAccent, href: "#!" }).html("link 4"))
             ])
           ])
         ])
       ),
       $('<div/>', {class: "footer-copyright"}).append(
-        $('<div/>', {class: function(){return "container " + colors.text + colors.textAccentHeader}}).html("© 2014 Copyright Text").append(
-          $('<a>', {class: function(){return "right " + colors.text + colors.textAccentHeader}}).html("More Links")
+        $('<div/>', {class: "container " + colors.text + colors.textAccentHeader}).html("© 2014 Copyright Text").append(
+          $('<a>', {class: "right " + colors.text + colors.textAccentHeader}).html("More Links")
         )
       )
     ])
@@ -96,13 +106,36 @@ function getFooter(){
 
 function animationActivate() {
   $(".dropdown-button").dropdown({
-       belowOrigin: true, // Displays dropdown below the button
-     });
-     $(".button-collapse").sideNav({
-       menuWidth: 330,
-       draggable: true,
-       closeOnClick: true,
-});
+    belowOrigin: true, // Displays dropdown below the button
+  });
+  $("#slide-out-button").sideNav({
+    menuWidth: 330,
+    draggable: true,
+    closeOnClick: true,
+  });
+}
+
+ // Start the app
+function initApp() {
+  getHeader()
+  getFooter()
+  animationActivate()
+
+  // Pull user Data
+  firebase.auth().onAuthStateChanged(function(user){
+    if (user){
+      console.log(firebase.auth().currentUser);
+      $("#user_name").html(user.displayName);
+      $("#user_photo").attr("src",user.photoURL)
+      $("#user_email").html(user.email);
+    }
+  })
+
+  // Activate LogOut
+  $("#logout_button").click(function(){
+    firebase.auth().signOut();
+    window.location = window.location;
+  });
 
 
 }
