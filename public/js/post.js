66fae107b4bbe1650ae3c3b639db8920ad7dcf35
Mhnd Mousa <mhndmsa@gmail.com>
2018-03-12 02:12:41 -0400

    post: title & description added

--- public/js/post.js
+++ public/js/post.js
@@ -3,14 +3,36 @@
 function getPostForm(){
   $('mpost').append(
     $('<div>', {class: "row"}).append(
-      $('<form>', {class: "col s12"}).append(
+      $('<form>', {class: "col s12"}).append([
         $('<div>', {class: "row"}).append(
           $('<div>', {class:"input-field col s6"}).append([
             $('<input>', {class: "validate", type:"text", id:"name"}),
-            $('<label>', {"for": "first_name"}).html("Name")
+            $('<label>', {"for": "title"}).html("Title")
+          ]),
+          $('<div>', {class:"input-field col s6"}).append([
+            $('<select>').append([
+              $('<option>', {value:"", 'selected':"", 'disabled': ""}).html("Choose your catagories"),
+              $('<option>', {value:"1"}).html("Graffiti"),
+              $('<option>', {value:"2"}).html("Home Work"),
+              $('<option>', {value:"3"}).html("Painting")
+
+            ])
           ])
+
+          // $('<div>', {class:"input-field col s6"}).append([
+          //   $('<input>', {class: "validate", type:"text", id:"name"}),
+          //   $('<label>', {"for": "email"}).html("Email")
+          // ])
+        ),
+        $('<div>', {class: "row"}).append(
+          $('<div>', {class:"input-field col s12"}).append([
+            $('<input>', {class: "validate", type:"text", id:"name"}),
+            $('<label>', {"for": "description"}).html("Description")
+          ])
+        ),
+        $('<div>', {class: "row"}).append(
         )
-      )
+      ])
     )
   )
 }
