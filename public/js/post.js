902d26a232a579d47b985045b6a9398e48e6ce8d
Mhnd Mousa <mhndmsa@gmail.com>
2018-03-12 01:41:39 -0400

    post: name form correct

--- public/js/post.js
+++ public/js/post.js
@@ -6,8 +6,8 @@ function getPostForm(){
       $('<form>', {class: "col s12"}).append(
         $('<div>', {class: "row"}).append(
           $('<div>', {class:"input-field col s6"}).append([
-            $('<input>', {class: "validate", type:"text", id:"name"})
-            $('<label>', {for: "first_name"}).html("Name")
+            $('<input>', {class: "validate", type:"text", id:"name"}),
+            $('<label>', {"for": "first_name"}).html("Name")
           ])
         )
       )
